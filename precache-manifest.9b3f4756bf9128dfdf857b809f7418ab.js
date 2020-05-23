9564fb968066174979fb60d8d679d969b2c87692
michael orlando <michael.john.orlando@gmail.com>
2020-05-23 16:36:34 -0500

    Updates

---
+++ precache-manifest.9b3f4756bf9128dfdf857b809f7418ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f97867e8ce0d8378a8974dff29c14b8",
+    "url": "/Mr-Orlando/index.html"
+  },
+  {
+    "revision": "d81b560d0242de6d881d",
+    "url": "/Mr-Orlando/static/css/main.5e4fdc0d.chunk.css"
+  },
+  {
+    "revision": "866ff772008f3c7e690d",
+    "url": "/Mr-Orlando/static/js/2.74d22fe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mr-Orlando/static/js/2.74d22fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81b560d0242de6d881d",
+    "url": "/Mr-Orlando/static/js/main.75290cdf.chunk.js"
+  },
+  {
+    "revision": "2145bd37c7b6f51b1e11",
+    "url": "/Mr-Orlando/static/js/runtime-main.297892c0.js"
+  },
+  {
+    "revision": "4307bc88eb59c865340c461c082419d0",
+    "url": "/Mr-Orlando/static/media/BarcodeText.4307bc88.woff"
+  }
+]);
